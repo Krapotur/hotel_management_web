465310729108763fdad970a27e1e86a58555382e
Krapotur <68387294+Krapotur@users.noreply.github.com>
2024-01-15 17:06:07 +0300

    Create Login Page Create layouts(auth, admin, user) Add routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,24 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {AuthLayoutComponent} from "./shared/layouts/auth-layout/auth-layout.component";
+import {LoginPageComponent} from "./login-page/login-page.component";
+import {AdminLayoutComponent} from "./shared/layouts/admin-layout/admin-layout.component";
+import {UserLayoutComponent} from "./shared/layouts/user-layout/user-layout.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: AuthLayoutComponent, children: [
+      {path: '', redirectTo: '/login', pathMatch: "full"},
+      {path: 'login', component: LoginPageComponent}
+    ]
+  },
+  {
+    path: 'admin', component: AdminLayoutComponent,  children: [
+
+    ]
+  },
+  {
+    path: 'user', component: UserLayoutComponent, children: [
+
+    ]
+  }
+];
