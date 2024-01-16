63205223be650f4f580dbb25212ffb4de4e337ef
Krapotur <68387294+Krapotur@users.noreply.github.com>
2024-01-16 17:00:55 +0300

    Add validation in form on Login page

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,17 +1,40 @@
-import {Component} from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import {MatInputModule} from "@angular/material/input";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatButtonModule} from "@angular/material/button";
 import {MatTooltipModule} from "@angular/material/tooltip";
+import {NgIf} from "@angular/common";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule, MatTooltipModule,],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatTooltipModule,
+    ReactiveFormsModule,
+    NgIf,
+    MatIconModule
+  ],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
-export class LoginPageComponent {
-  value = 'Логин';
+export class LoginPageComponent implements OnInit{
+  form: FormGroup
+  protected readonly onsubmit = onsubmit;
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      login: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  onSubmit() {
+
+  }
 }
