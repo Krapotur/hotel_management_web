b54c1d71931d2e9c80188df1094eb40203905d0c
Krapotur <68387294+Krapotur@users.noreply.github.com>
2024-01-18 16:09:02 +0300

    Add sidebar in admin page

--- client/src/app/admin-page/admin-page.component.ts
+++ client/src/app/admin-page/admin-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-admin-page',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './admin-page.component.html',
   styleUrl: './admin-page.component.scss'
 })
