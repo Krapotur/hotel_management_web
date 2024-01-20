be7836df3f5a8eafb5f082c46210c52a8e2f34d0
Krapotur <68387294+Krapotur@users.noreply.github.com>
2024-01-20 17:27:11 +0300

    Edit users page

--- client/src/app/admin-pages/groups-page/groups-page.component.ts
+++ client/src/app/admin-pages/groups-page/groups-page.component.ts
@@ -2,30 +2,39 @@ import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatPaginator, MatPaginatorModule} from "@angular/material/paginator";
 import {MatTableDataSource, MatTableModule} from "@angular/material/table";
 import {Group} from "../../shared/interfaces";
+import {MatButtonModule} from "@angular/material/button";
+import {NgForOf, NgIf} from "@angular/common";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-groups-page',
   standalone: true,
-    imports: [
-        MatPaginatorModule,
-        MatTableModule
-    ],
+  imports: [
+    MatPaginatorModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    NgIf,
+    NgForOf,
+    MatInputModule,
+    MatSelectModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './groups-page.component.html',
   styleUrl: './groups-page.component.scss'
 })
 export class GroupsPageComponent implements AfterViewInit {
   ELEMENT_DATA: Group[] = [
-    {position: 1, name: 'Заливина Анна', post: 'Руководитель', phone: '79895423544'},
-    {position: 2, name: 'Администратор гостиниц', post: 'Администратор гостиниц', phone: '79895423544'},
-    {position: 3, name: 'Администратор системы', post: 'Администратор системы', phone: '79895423544'},
-    {position: 4, name: 'Рахмонова Оля', post: 'Горничная', phone: '79895423544'},
-    {position: 5, name: 'Аметова Замира', post: 'Горничная', phone: '79895423544'},
-    {position: 6, name: 'Толибова Феруза', post: 'Горничная', phone: '79895423544'},
-    {position: 7, name: 'Гафарова Хабиба', post: 'Горничная', phone: '79895423544'},
+    {position: 1, post: 'Руководитель'},
+    {position: 2, post: 'Администратор гостиниц'},
+    {position: 3, post: 'Администратор системы'},
+    {position: 4, post: 'Горничная'},
   ];
 
 
-  displayedColumns: string[] = ['#', 'name', 'post', 'phone'];
+  displayedColumns: string[] = ['#', 'post'];
   dataSource = new MatTableDataSource<Group>(this.ELEMENT_DATA);
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
